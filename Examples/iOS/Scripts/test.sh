c3110558a697fcad6c93b9509d8bc640b6812924
Sam Deane <sam@elegantchaos.com>
2012-02-22 23:54:05 +0000

    moved ios example, fixed build scripts

--- Examples/iOS/Scripts/test.sh
+++ Examples/iOS/Scripts/test.sh
@@ -1,7 +1,7 @@
-echo Testing Mac ECLoggingSample xx
+echo Testing iOS ECLoggingSample
 
 base=`dirname $0`
 source "$base/../../../Tests/ECUnitTests/Scripts/test-common.sh"
 
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingMac"
+xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingIOS"
 xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSample"
