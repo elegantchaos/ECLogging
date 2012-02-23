57927b22659c3e221f2d57bbb1a24c18c93101de
Sam Deane <sam@elegantchaos.com>
2012-02-23 00:03:03 +0000

    added sdk to test script

--- Examples/Mac/Scripts/test.sh
+++ Examples/Mac/Scripts/test.sh
@@ -3,5 +3,5 @@ echo Testing Mac ECLoggingSample xx
 base=`dirname $0`
 source "$base/../../../Tests/ECUnitTests/Scripts/test-common.sh"
 
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingMac"
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSample"
+xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingMac" -sdk "$testSDKMac"
+xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSample" -sdk "$testSDKMac"
