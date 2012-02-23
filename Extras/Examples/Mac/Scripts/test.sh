eb39b3bb211bc13e3fc1f51fcfdb64597de40a2f
Sam Deane <sam@elegantchaos.com>
2012-02-23 10:33:45 +0000

    fixed build scripts

--- Extras/Examples/Mac/Scripts/test.sh
+++ Extras/Examples/Mac/Scripts/test.sh
@@ -4,4 +4,4 @@ base=`dirname $0`
 source "$base/../../../Tests/ECUnitTests/Scripts/test-common.sh"
 
 xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingMac" -sdk "$testSDKMac"
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSample" -sdk "$testSDKMac"
+xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSampleMac" -sdk "$testSDKMac"
--- Extras/Examples/iOS/Scripts/test.sh
+++ Extras/Examples/iOS/Scripts/test.sh
@@ -4,4 +4,4 @@ base=`dirname $0`
 source "$base/../../../Tests/ECUnitTests/Scripts/test-common.sh"
 
 xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingIOS" -sdk "$testSDKiOS"
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSample" -sdk "$testSDKiOS"
+xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSampleIOS" -sdk "$testSDKiOS"
