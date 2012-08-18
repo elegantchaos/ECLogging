ac069f445200cb763660dce8bb5705fe359eb978
Sam Deane <sam@elegantchaos.com>
2012-08-18 17:27:30 +0100

    tweaked main header

--- Source/Generic/ECLogging.h
+++ Source/Generic/ECLogging.h
@@ -1,14 +1,10 @@
 // --------------------------------------------------------------------------
-//! @author Sam Deane
-//! @date 12/04/2011
-//
-//! @file:
-//! Logging utilities.
-//
 //  Copyright 2012 Sam Deane, Elegant Chaos. All rights reserved.
 //  This source code is distributed under the terms of Elegant Chaos's 
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
+
+#include <ECConfig/ECConfig.h>
 
 #import "ECLoggingMacros.h"
 #import "ECAssertion.h"
