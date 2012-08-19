ff527716992e69cb4573ead1def9db84a4f8c826
Sam Deane <sam@elegantchaos.com>
2012-08-19 19:01:58 +0100

    added platform specific ECLogging.h file

---
+++ Source/iOS/ECLogging.h
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------------
+//  Copyright 2012 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's 
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
+
+#import "ECLoggingGeneric.h"
+
+#import "ECErrorPresenterHandler.h"
+#import "ECDebugChannelsViewController.h"
+#import "ECDebugChannelViewController.h"
+#import "ECDebugViewController.h"
+#import "ECDebugViewPopoverController.h"
+#import "ECLogViewHandler.h"
+#import "ECLogViewHandlerItem.h"
