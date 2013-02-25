07e3c93f3c9d52b03b95055d45ad3d691ec860f5
Sam Deane <sam@elegantchaos.com>
2013-02-25 21:15:26 +0000

    Removed extra copy of examples.

--- Extras/Examples/Mac/Scripts/test.sh
+++
@@ -1,7 +0,0 @@
-echo Testing ECLoggingSampleMac
-
-base=`dirname $0`
-source "$base/../../../Tests/ECUnitTests/Scripts/test-common.sh"
-
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingMac" -sdk "$testSDKMac"
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSampleMac" -sdk "$testSDKMac"
--- Extras/Examples/iOS/Scripts/test.sh
+++
@@ -1,7 +0,0 @@
-echo Testing iOS ECLoggingSample
-
-base=`dirname $0`
-source "$base/../../../Tests/ECUnitTests/Scripts/test-common.sh"
-
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingIOS" -sdk "$testSDKiOS"
-xcodebuild -workspace "ECLoggingSample.xcworkspace" -scheme "ECLoggingSampleIOS" -sdk "$testSDKiOS"
