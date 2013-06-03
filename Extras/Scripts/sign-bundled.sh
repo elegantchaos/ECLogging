edbb76d789b97a17dc92a6286bf1d90dec4fc90b
Sam Deane <sam@elegantchaos.com>
2013-06-03 19:56:25 +0100

    Improved re-signing script. Now checks the identity and certificate used to sign, and doesn't bother doing it again if they match.

--- Extras/Scripts/sign-bundled.sh
+++ Extras/Scripts/sign-bundled.sh
@@ -1,4 +1,40 @@
 
+sign()
+{
+    BUNDLEID="$1"
+    CODE_SIGN_IDENTITY="$2"
+    FILE="$3"
+
+    # get current signing details
+    CURRENT=`codesign --verbose=2 -d "${FILE}" 2>&1`
+
+#   echo "current:$CURRENT"
+
+    # get current id
+    PATTERN="Identifier=([a-zA-Z0-9.]*)"
+    [[ "$CURRENT" =~ $PATTERN ]]
+    CURRENT_IDENTIFIER=${BASH_REMATCH[1]}
+
+    # get first authority
+    PATTERN="Authority=([a-zA-Z0-9: ]*)"
+    [[ "$CURRENT" =~ $PATTERN ]]
+    CURRENT_AUTHORITY=${BASH_REMATCH[1]}
+
+#    echo "current:$CURRENT_IDENTIFIER required:$BUNDLEID"
+#    echo "current:$CURRENT_AUTHORITY required:$CODE_SIGN_IDENTITY"
+
+    NAME=`basename "$FILE"`
+    echo
+
+    # check if we need to resign (resigning can be slow, so we check first)
+    if [[ ("$CURRENT_IDENTIFIER" != "$BUNDLEID") || ("$CURRENT_AUTHORITY" != "$CODE_SIGN_IDENTITY"*) ]] ; then
+        echo "Resigning $NAME with id $BUNDLEID"
+        codesign -f -i ${BUNDLEID} -vv -s "${CODE_SIGN_IDENTITY}" "${FILE}"
+    else
+        echo "Didn't need to sign $NAME - already signed correctly"
+    fi
+}
+
 ## Re-sign every plugin and framework using the bundle id and code signing identity of the target
 ##
 ## You can use this script in a Run Script phase to ensure that all plugins and frameworks are signed consistently.
@@ -26,10 +62,7 @@ if [ -e "${CODESIGNING_FOLDER_PATH}/Contents/PlugIns/" ]; then
 		if [[ "$BUNDLEID" == "" ]]; then
 			BUNDLEID="$APPID"
 		fi
-		NAME=`basename "$f"`
-		echo
-		echo "Resigning $NAME with id $BUNDLEID"
-	    codesign -f -i ${BUNDLEID} -vv -s "${CODE_SIGN_IDENTITY}" "$f"
+	    sign "${BUNDLEID}" "${CODE_SIGN_IDENTITY}" "$f"
 	done
 fi
 
@@ -41,10 +74,7 @@ if [ -e "${CODESIGNING_FOLDER_PATH}/Contents/Frameworks/" ]; then
 		if [[ $? != 0 ]]; then
 			BUNDLEID="$APPID"
 		fi
-		NAME=`basename "$f"`
-		echo " "
-		echo "Resigning $NAME with id $BUNDLEID"
-	    codesign -f -i ${BUNDLEID} -vv -s "${CODE_SIGN_IDENTITY}" "$f" > /dev/null
+        sign "${BUNDLEID}" "${CODE_SIGN_IDENTITY}" "$f"
 	done
 fi
 
