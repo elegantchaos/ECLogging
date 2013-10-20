ada5cbadb3cd8b2b0e6737c2378875b2a161c5a3
Sam Deane <sam@elegantchaos.com>
2013-10-20 11:12:47 +0100

    signing tweaks

--- Extras/Scripts/verify-app-signing.sh
+++ Extras/Scripts/verify-app-signing.sh
@@ -5,6 +5,11 @@
 
 app="$1"
 opts="--verbose --deep"
+
+echo "spctl:"
+spctl --verbose=8 --assess --type execute "$app"
+
+echo "codesign:"
 codesign -v "$app" $opts
-#codesign -v "$app/Contents/Frameworks/"* $opts
-#codesign -v "$app/Contents/PlugIns/"* $opts
+codesign -v "$app/Contents/Frameworks/"* $opts
+codesign -v "$app/Contents/PlugIns/"* $opts
