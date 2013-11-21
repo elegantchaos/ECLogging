2b24cd1ec9a2ab73db742e40bb5b4175207599a5
Sam Deane <sam@elegantchaos.com>
2013-11-21 12:09:44 +0000

    implemented dictionary comparisons, tweaked output format

--- Source/Generic/ECTestComparisons.m
+++ Source/Generic/ECTestComparisons.m
@@ -16,14 +16,14 @@
 	Class c2 = [item2 class];
 	NSString* context = nil;
 	if (c1 == c2)
-		context = NSStringFromClass(c1);
+		context = [NSString stringWithFormat:@"%@", c1];
 	else
 		context = [NSString stringWithFormat:@"%@ vs %@", c1, c2];
 	
-	return [self matches:item2 context:context indent:@"" block:block];
+	return [self matches:item2 context:context level:0 block:block];
 }
 
-- (BOOL)matches:(id)item2 context:(NSString *)context indent:(NSString*)indent block:(ECTestComparisonBlock)block
+- (BOOL)matches:(id)item2 context:(NSString *)context level:(NSUInteger)level block:(ECTestComparisonBlock)block
 {
 	BOOL matches = [self isKindOfClass:[item2 class]];
 	if (matches)
@@ -32,7 +32,7 @@
 	}
 
 	if (!matches)
-		block(context, self, item2);
+		block(context, level, self, item2);
 
 	return matches;
 }
@@ -41,16 +41,13 @@
 
 @implementation NSArray(ECTestComparisons)
 
-- (BOOL)matches:(id)item2 context:(NSString *)context indent:(NSString *)indent block:(ECTestComparisonBlock)block
+- (BOOL)matches:(id)item2 context:(NSString *)context level:(NSUInteger)level block:(ECTestComparisonBlock)block
 {
-	indent = [indent stringByAppendingString:@"\t"];
-	NSString* newContext = [NSString stringWithFormat:@"%@\n%@array", context, indent];
-	
 	BOOL matches = [self isKindOfClass:[NSArray class]];
 	if (!matches)
 	{
-		newContext = [NSString stringWithFormat:@"%@ compared with %@", newContext, [item2 class]];
-		block(newContext, self, item2);
+		NSString* newContext = [NSString stringWithFormat:@"%@ NSArray compared with %@", context, [item2 class]];
+		block(newContext, level, self, item2);
 	}
 	else
 	{
@@ -60,8 +57,8 @@
 
 		for (NSUInteger n = 0; n < min; ++n)
 		{
-			NSString* itemContext = [NSString stringWithFormat:@"%@ item %ld", newContext, n];
-			matches = [self[n] matches:item2[n] context:itemContext indent:indent block:block] && matches;
+			NSString* itemContext = [NSString stringWithFormat:@"%@[%ld]", context, n];
+			matches = [self[n] matches:item2[n] context:itemContext level:level+1 block:block] && matches;
 		}
 
 		if (c1 < c2)
@@ -69,8 +66,8 @@
 			matches = NO;
 			for (NSUInteger n = min; n < c2; ++n)
 			{
-				newContext = [NSString stringWithFormat:@"%@ extra item %ld", newContext, n];
-				block(newContext, nil, item2[n]);
+				NSString* itemContext = [NSString stringWithFormat:@"%@[%ld] extra item", context, n];
+				block(itemContext, level, nil, item2[n]);
 			}
 		}
 
@@ -79,9 +76,53 @@
 			matches = NO;
 			for (NSUInteger n = min; n < c1; ++n)
 			{
-				newContext = [NSString stringWithFormat:@"%@ missing item %ld", newContext, n];
-				block(newContext, self[n], nil);
+				NSString* itemContext = [NSString stringWithFormat:@"%@[%ld] missing item", context, n];
+				block(itemContext, level, self[n], nil);
 			}
+		}
+	}
+	
+	return matches;
+}
+
+@end
+
+@implementation NSDictionary(ECTestComparisons)
+
+- (BOOL)matches:(id)item2 context:(NSString *)context level:(NSUInteger)level block:(ECTestComparisonBlock)block
+{
+	BOOL matches = [self isKindOfClass:[NSDictionary class]];
+	if (!matches)
+	{
+		NSString* newContext = [NSString stringWithFormat:@"%@ NSDictionary compared with %@", context, [item2 class]];
+		block(newContext, level, self, item2);
+	}
+	else
+	{
+		NSMutableArray* keys2 = [NSMutableArray arrayWithArray:[item2 allKeys]];
+		for (NSString* key in self)
+		{
+			id v1 = self[key];
+			id v2 = item2[key];
+			if (v2)
+			{
+				NSString* itemContext = [NSString stringWithFormat:@"%@[@\"%@\"]", context, key];
+				matches = [v1 matches:v2 context:itemContext level:level+1 block:block] && matches;
+			}
+			else
+			{
+				NSString* itemContext = [NSString stringWithFormat:@"%@[@\"%@\"] missing key", context, key];
+				block(itemContext, level, v1, nil);
+				matches = NO;
+			}
+			[keys2 removeObject:key];
+		}
+		
+		for (NSString* key in keys2)
+		{
+			NSString* itemContext = [NSString stringWithFormat:@"%@[@\"%@\"] extra key", context, key];
+			block(itemContext, level, nil, item2[key]);
+			matches = NO;
 		}
 	}
 	
--- Source/Tests/ComparisonTests.m
+++ Source/Tests/ComparisonTests.m
@@ -17,11 +17,12 @@
 - (BOOL)item:(id)item1 matches:(id)item2
 {
 	NSMutableString* string = [NSMutableString new];
-	BOOL result = [item1 matches:item2 block:^(NSString *context, id i1, id i2) {
+	BOOL result = [item1 matches:item2 block:^(NSString *context, NSUInteger level, id i1, id i2) {
 		[string appendFormat:@"%@: %@ didn't match %@\n", context, i1, i2];
 	}];
 
 	self.output = string;
+	NSLog(@"Output:\n%@", string);
 	
 	return result;
 }
@@ -51,7 +52,7 @@
 	NSArray* a1 = @[@"abc", @"def"];
 	NSArray* a2 = @[@"def", @"abc"];
 	ECTestAssertFalse([self item:a1 matches:a2]);
-	ECTestAssertIsEqual(self.output, @"__NSArrayI\n\tarray item 0: abc didn't match def\n__NSArrayI\n\tarray item 1: def didn't match abc\n");
+	ECTestAssertIsEqual(self.output, @"__NSArrayI[0]: abc didn't match def\n__NSArrayI[1]: def didn't match abc\n");
 }
 
 - (void)testArraysShorter
@@ -59,7 +60,7 @@
 	NSArray* a1 = @[@"abc", @"def"];
 	NSArray* a2 = @[@"abc"];
 	ECTestAssertFalse([self item:a1 matches:a2]);
-	ECTestAssertIsEqual(self.output, @"__NSArrayI\n\tarray missing item 1: def didn't match (null)\n");
+	ECTestAssertIsEqual(self.output, @"__NSArrayI[1] missing item: def didn't match (null)\n");
 }
 
 - (void)testArraysLonger
@@ -67,7 +68,7 @@
 	NSArray* a1 = @[@"abc"];
 	NSArray* a2 = @[@"abc", @"def"];
 	ECTestAssertFalse([self item:a1 matches:a2]);
-	ECTestAssertIsEqual(self.output, @"__NSArrayI\n\tarray extra item 1: (null) didn't match def\n");
+	ECTestAssertIsEqual(self.output, @"__NSArrayI[1] extra item: (null) didn't match def\n");
 }
 
 - (void)testArraysDifferentAndLonger
@@ -75,7 +76,47 @@
 	NSArray* a1 = @[@"abc"];
 	NSArray* a2 = @[@"def", @"abc"];
 	ECTestAssertFalse([self item:a1 matches:a2]);
-	ECTestAssertIsEqual(self.output, @"__NSArrayI\n\tarray item 0: abc didn't match def\n__NSArrayI\n\tarray extra item 1: (null) didn't match abc\n");
+	ECTestAssertIsEqual(self.output, @"__NSArrayI[0]: abc didn't match def\n__NSArrayI[1] extra item: (null) didn't match abc\n");
+}
+
+- (void)testDictionariesEqual
+{
+	NSDictionary* d1 = @{@"k1":@"abc", @"k2":@"def"};
+	NSDictionary* d2 = @{@"k1":@"abc", @"k2":@"def"};
+	ECTestAssertTrue([self item:d1 matches:d2]);
+	ECTestAssertIsEqual(self.output, @"");
+}
+
+- (void)testDictionariesDifferent
+{
+	NSDictionary* d1 = @{@"k1":@"abc", @"k2":@"def"};
+	NSDictionary* d2 = @{@"k1":@"def", @"k2":@"abc"};
+	ECTestAssertFalse([self item:d1 matches:d2]);
+	ECTestAssertIsEqual(self.output, @"__NSDictionaryI[@\"k2\"]: def didn't match abc\n__NSDictionaryI[@\"k1\"]: abc didn't match def\n");
+}
+
+- (void)testDictionariesShorter
+{
+	NSDictionary* d1 = @{@"k1":@"abc", @"k2":@"def"};
+	NSDictionary* d2 = @{@"k1":@"abc"};
+	ECTestAssertFalse([self item:d1 matches:d2]);
+	ECTestAssertIsEqual(self.output, @"__NSDictionaryI[@\"k2\"] missing key: def didn't match (null)\n");
+}
+
+- (void)testDictionariesLonger
+{
+	NSDictionary* d1 = @{@"k1":@"abc"};
+	NSDictionary* d2 = @{@"k1":@"abc", @"k2":@"def"};
+	ECTestAssertFalse([self item:d1 matches:d2]);
+	ECTestAssertIsEqual(self.output, @"__NSDictionaryI[@\"k2\"] extra key: (null) didn't match def\n");
+}
+
+- (void)testDictionariesDifferentAndLonger
+{
+	NSDictionary* d1 = @{@"k1":@"abc"};
+	NSDictionary* d2 = @{@"k1":@"def", @"k2":@"abc"};
+	ECTestAssertFalse([self item:d1 matches:d2]);
+	ECTestAssertIsEqual(self.output, @"__NSDictionaryI[@\"k1\"]: abc didn't match def\n__NSDictionaryI[@\"k2\"] extra key: (null) didn't match abc\n");
 }
 
 @end
