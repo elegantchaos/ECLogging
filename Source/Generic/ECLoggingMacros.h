1e297da0045cf9243548e67d11d7d9f0272eac23
Sam Deane <sam@elegantchaos.com>
2014-04-11 17:49:04 +0100

    Updated copyright date.

--- Source/Generic/ECLoggingMacros.h
+++ Source/Generic/ECLoggingMacros.h
@@ -3,7 +3,7 @@
 //! @file:
 //! Logging utilities.
 //
-//  Copyright 2013 Sam Deane, Elegant Chaos. All rights reserved.
+//  Copyright 2014 Sam Deane, Elegant Chaos. All rights reserved.
 //  This source code is distributed under the terms of Elegant Chaos's 
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
