c27d8925b836c6869920ab6ccfaab0b2a50aac5e
Sam Deane <sam@elegantchaos.com>
2014-08-28 18:27:33 +0100

    Improved app signing validation script.

--- Extras/Scripts/verify-app-signing.sh
+++ Extras/Scripts/verify-app-signing.sh
@@ -4,15 +4,54 @@
 ## Takes the path to the app as the first parameter.
 
 app="$1"
-opts="--verbose --deep"
 
 echo "spctl:"
 spctl --verbose=8 --assess --type execute "$app"
 
-echo "codesign:"
-codesign -v "$app" $opts
-codesign -v "$app/Contents/Frameworks/"* $opts
-if [[ -e "$app/Contents/PlugIns/" ]]
-then
-    codesign -v "$app/Contents/PlugIns/"* $opts
+local -a items=( "$app" )
+echo $items
+for f in "$app/Contents/Frameworks/"*; do items+=("$f"); done
+
+if [[ -e "$app/Contents/PlugIns/" ]]; then
+    for f in "$app/Contents/PlugIns/"*; do items+=("$f"); done
 fi
+
+if [[ -e "$app/Contents/Library/Quicklook/" ]]; then
+    for f in "$app/Contents/Library/Quicklook/"*; do items+=("$f"); done
+fi
+
+echo ${items[@]}
+
+
+echo ""
+echo "Signing:"
+echo "--------"
+echo ""
+
+for f in ${items[@]}
+do
+    echo "Checking $f"
+    codesign --verbose=5 --deep --verify "$f"
+    echo ""
+done
+
+echo "Entitlements:"
+echo "-------------"
+echo ""
+
+for f in ${items[@]}
+do
+    echo "Entitlements for $f"
+    codesign -vvv --display --entitlements :- "$f"
+    echo ""
+done
+
+#codesign --display --entitlements :- "$app" $opts
+#codesign -v "$app/Contents/Frameworks/"* $opts
+#if [[ -e "$app/Contents/PlugIns/" ]]
+#then
+#codesign -v "$app/Contents/PlugIns/"* $opts
+#fi
+
+
+#codesign -dvvv --entitlements :- executable_path
