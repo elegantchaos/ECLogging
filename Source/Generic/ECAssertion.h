e084dd3ef2c4fe7ae7aa0f6d041546c67f64aef5
Sam Deane <sam@elegantchaos.com>
2015-01-24 11:53:45 +0000

    Updated copyrights.

--- Source/Generic/ECAssertion.h
+++ Source/Generic/ECAssertion.h
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------
 //
-//  Copyright (c) 2014 Sam Deane, Elegant Chaos. All rights reserved.
+//  Copyright (c) 2015 Sam Deane, Elegant Chaos. All rights reserved.
 //  This source code is distributed under the terms of Elegant Chaos's 
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
