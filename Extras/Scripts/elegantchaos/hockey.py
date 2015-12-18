91cfeca1aff9bf4a9ee4ce6d9bc05d51757a20ee
Sam Deane <sam@elegantchaos.com>
2015-12-18 17:24:21 +0000

    Work in progress, implementing simple hockey library.

--- Extras/Scripts/elegantchaos/hockey.py
+++ Extras/Scripts/elegantchaos/hockey.py
@@ -3,9 +3,8 @@
 
 import os
 import keychain
-
-from hockeylib import app, crash
-
+import urllib2
+import json
 
 def set_token(token):
     keychain.set_internet_password("hockey-api-key", token, "api.hockeyapp.net")
@@ -13,11 +12,35 @@ def set_token(token):
 def get_token():
     return keychain.get_internet_password("api.hockeyapp.net")
 
+def hockey_request(command, token):
+    url = "https://rink.hockeyapp.net/api/2/" + command
+    request = urllib2.Request(url)
+    request.add_header('X-HockeyAppToken', token)
+    return request
+
+def get_apps(token):
+    request = hockey_request('apps', token)
+    response = urllib2.urlopen(request)
+    outputJSON = response.read()
+    output = json.loads(outputJSON)
+    return output
+
+def get_app_versions(token, appid):
+    request = hockey_request('apps/' + appid + "/app_versions", token)
+    response = urllib2.urlopen(request)
+    outputJSON = response.read()
+    output = json.loads(outputJSON)
+    return output
+
+def get_app_statistics(token, appid):
+    request = hockey_request('apps/' + appid + "/statistics", token)
+    response = urllib2.urlopen(request)
+    outputJSON = response.read()
+    output = json.loads(outputJSON)
+    return output
+
 if __name__ == '__main__':
     (user, token) = get_token()
 
-    os.environ['HOCKEY_API_KEY'] = token
-
-    apps = app.get_all_apps()
-    for app in apps:
-        print "Found app {0}".format(app.title())
+    print get_app_statistics(token, '1ee03b2c845f45f7b7564123f5283409')
+    # print [(a['title'], a['id']) for a in get_apps(token).get('apps')]
