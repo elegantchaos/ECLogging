261499851e229db4ac60766c73b8be6e03e996a4
Sam Deane <sam@elegantchaos.com>
2015-12-18 14:52:31 +0000

    fixed formatting error

--- Extras/Scripts/elegantchaos/git.py
+++ Extras/Scripts/elegantchaos/git.py
@@ -286,8 +286,8 @@ def cleanup_local_branch(branch, tags, forced = False):
             else:
                 shell.log_verbose(output)
         elif (localCommit != remoteCommit):
-            shell.log_verbose("Skipped {0} as it's not fully pushed to the server.")
-            
+            shell.log_verbose("Skipped {0} as it's not fully pushed to the server.".format(branch))
+
 
 
 def enumerate_submodules(cmd, args = None):
