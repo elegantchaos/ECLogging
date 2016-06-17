c9ddab146b14295afc79bce31de744fe2f6cea2a
Sam Deane <sam@elegantchaos.com>
2016-06-17 12:27:27 -0700

    Added swift test which inherits from ECTestCase and actually works (deliberately fails, currently)

--- Source/Tests/SwiftTests.swift
+++ Source/Tests/SwiftTests.swift
@@ -7,8 +7,9 @@
 //
 
 import XCTest
+import ECUnitTests
 
-class SwiftTests: XCTestCase {
+class SwiftTests: ECTestCase {
 
     override func setUp() {
         super.setUp()
@@ -23,6 +24,8 @@ class SwiftTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+		self.assertString("blah", matchesString: "blah2")
+
     }
 
     func testPerformanceExample() {
