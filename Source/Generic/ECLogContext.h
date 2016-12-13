20d7247d22fa17a19ea51644d5288bfb5baf64bd
Sam Deane <sam@elegantchaos.com>
2016-12-13 08:41:21 +0000

    Updated headers.

--- Source/Generic/ECLogContext.h
+++ Source/Generic/ECLogContext.h
@@ -1,6 +1,5 @@
 // --------------------------------------------------------------------------
-//
-//  Copyright 2014 Sam Deane, Elegant Chaos. All rights reserved.
+//  Copyright 2016 Elegant Chaos Limited. All rights reserved.
 //  This source code is distributed under the terms of Elegant Chaos's 
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
--- Source/Generic/ECLogging.m
+++ Source/Generic/ECLogging.m
@@ -1,9 +1,5 @@
 // --------------------------------------------------------------------------
-//
-//! @file:
-//! Logging utilities.
-//
-//  Copyright 2014 Sam Deane, Elegant Chaos. All rights reserved.
+//  Copyright 2016 Elegant Chaos Limited. All rights reserved.
 //  This source code is distributed under the terms of Elegant Chaos's
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
