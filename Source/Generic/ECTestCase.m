1152faf26a5da003a99b462f72e544d2b9764412
Gavin MacLean <gavin@openplanetsoftware.com>
2018-05-03 13:30:05 +0100

    Ensure that we don't get hit with the PNG gamma bug when exporting reference images #17764

--- Source/Generic/ECTestCase.m
+++ Source/Generic/ECTestCase.m
@@ -658,7 +658,7 @@
 - (NSURL*)writeOutputImage:(NSBitmapImageRep*)image name:(NSString*)name asReference:(BOOL)asReference {
 	NSURL* desktop = [self URLForOutputAsReference:asReference];
 	NSURL* file = [[desktop URLByAppendingPathComponent:name] URLByAppendingPathExtension:@"png"];
-	NSData* data = [image representationUsingType:NSPNGFileType properties:@{NSImageInterlaced: @YES}];
+	NSData* data = [image PNGRepresentationWithInterlaced:YES];
 	[[NSFileManager defaultManager] createDirectoryAtURL:[file URLByDeletingLastPathComponent] withIntermediateDirectories:YES attributes:@{} error:nil];
 	NSError* error = nil;
 	BOOL written = [data writeToURL:file options:NSDataWritingAtomic error:&error];
@@ -668,8 +668,8 @@
 
 - (BOOL)imageAsPNG:(NSBitmapImageRep*)image exactlyMatchesReferenceImageAsPNG:(NSBitmapImageRep*)reference
 {
-	NSData* imageData = [image representationUsingType:NSPNGFileType properties:@{ NSImageInterlaced: @(YES) }];
-	NSData* referenceData = [reference representationUsingType:NSPNGFileType properties:@{ NSImageInterlaced: @(YES) }];
+	NSData* imageData = [image PNGRepresentationWithInterlaced:YES];
+	NSData* referenceData = [reference PNGRepresentationWithInterlaced:YES];
 	return [imageData isEqual:referenceData];
 }
 
